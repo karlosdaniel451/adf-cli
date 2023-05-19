49b7065525060de36905b8b0abfd9e7285934042
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2023-05-19 19:53:47 -0300

    feat: Adiciona a hierarquia inicial de comandos
    Adiciona o código de scaffolding do pacote de CLI Cobra em conjunto com
    o pacote de configuração Viper.
    
    Adiciona os comandos iniciais.
    
    Adiciona o arquivo de LICENSE.

---
+++ cmd/listRemote.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// listRemoteCmd represents the listRemote command
+var listRemoteCmd = &cobra.Command{
+	Use:   "listRemote",
+	Short: "Lista as versões disponíveis do ADF Web para instalação",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Versões disponíveis:")
+		fmt.Println("ADF Web 0.0.4 - latest")
+		fmt.Println("ADF Web 0.0.3")
+		fmt.Println("ADF Web 0.0.2")
+		fmt.Println("ADF Web 0.0.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listRemoteCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listRemoteCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listRemoteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
